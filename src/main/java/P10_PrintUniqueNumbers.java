4ed86f79035449a1ca1240ffffa93c5e7da45737
ShouvikSQA <shouvik9292@gmail.com>
2024-09-27 20:09:56 +0600

    my commit

--- src/main/java/P10_PrintUniqueNumbers.java
+++ src/main/java/P10_PrintUniqueNumbers.java
@@ -1,10 +1,20 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class P10_PrintUniqueNumbers {
     public static void main(String[] args) {
 
-        int[] array = {1, 1, 2, 3, 1, 2, 4, 5, 6, 5, 4, 7};
+        int[] array = new int[10];
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter The Numbers : ");
+        for (int i=0 ; i<array.length ; i++)
+        {
+              array[i] = sc.nextInt();
+        }
+
+        System.out.println();
 
         HashMap<Integer,Integer> mp = new HashMap<>();
 
